a56ed9122099803752b54209ac0ece4962e7ef00
Arthur Pachachura <pachachura.arthur@gmail.com>
2015-12-01 13:59:45 -0600

    Added vision to FTC9874 Library

--- sample/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ sample/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -59,5 +59,7 @@ public class FtcOpModeRegister implements OpModeRegister {
         manager.register("OptionsSample", OptionsSample.class);
         manager.register("FTC9874 DriveTrain", DriveTrain9874.class);
         manager.register("Double Joystick 9874", DoubleJoystick9874.class);
+	    manager.register("Basic Vision Sample", BasicVisionSample.class);
+	    manager.register("Manual Vision Sample", ManualVisionSample.class);
     }
 }
