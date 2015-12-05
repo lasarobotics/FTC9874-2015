a343f0eb96f715c44a2bdea2f62c12bbc94d35cf
Russell Coleman <russellsayshi@gmail.com>
2015-12-05 16:37:02 +0000

    Registered new opmodes

--- sample/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ sample/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -60,5 +60,7 @@ public class FtcOpModeRegister implements OpModeRegister {
         manager.register("FTC9874 DriveTrain", DriveTrain9874.class);
         manager.register("Double Joystick 9874", DoubleJoystick9874.class);
         manager.register("Motor Calibration", MotorNameTest.class);
+        manager.register("Easy Autonomous 9874", EasyAutonomous9874.class);
+        manager.register("Test Opmode 9874", TestOpmode.class);
     }
 }
