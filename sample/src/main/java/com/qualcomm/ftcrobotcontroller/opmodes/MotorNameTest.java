3d676051904d2d5df7e0b13b6143ec4d7fbd3ba0
Russell Coleman <russellsayshi@gmail.com>
2015-12-05 15:12:46 +0000

    Improved MotorNameTest.java

--- sample/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MotorNameTest.java
+++ sample/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MotorNameTest.java
@@ -4,13 +4,18 @@ import com.lasarobotics.library.controller.ButtonState;
 import com.lasarobotics.library.controller.Controller;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
- * Created by Will on 12/5/2015.
+ * Test the motors
  */
 public class MotorNameTest extends OpMode {
-    DcMotor leftFront, leftBack, rightBack, rightFront;
+    DcMotor leftFront, leftBack, rightBack, rightFront, arm, armEnd;
+    Servo servo;
     Controller one;
+
+    public static final double ON_POWER = 0.2;
+    private int servoAmount = 0;
 
     @Override
     public void stop() {
@@ -19,21 +24,40 @@ public class MotorNameTest extends OpMode {
     @Override
     public void loop() {
         if(one.a == ButtonState.PRESSED)
-            leftFront.setPower(0.5);
+            leftFront.setPower(ON_POWER);
         else
             leftFront.setPower(0);
         if(one.b == ButtonState.PRESSED)
-            leftBack.setPower(0.5);
+            leftBack.setPower(ON_POWER);
         else
             leftBack.setPower(0);
         if(one.x == ButtonState.PRESSED)
-            rightFront.setPower(0.5);
+            rightFront.setPower(ON_POWER);
         else
             rightFront.setPower(0);
         if(one.y == ButtonState.PRESSED)
-            rightBack.setPower(0.5);
+            rightBack.setPower(ON_POWER);
         else
             rightBack.setPower(0);
+        if(one.dpad_down == ButtonState.PRESSED)
+            arm.setPower(ON_POWER);
+        else
+            arm.setPower(0);
+        if(one.dpad_up == ButtonState.PRESSED)
+            armEnd.setPower(ON_POWER);
+        else
+            armEnd.setPower(0);
+        if(one.dpad_left == ButtonState.PRESSED) {
+            if(--servoAmount < 0) {
+                servoAmount = 0;
+            }
+        }
+        if(one.dpad_right == ButtonState.PRESSED) {
+            if(++servoAmount > 255) {
+                servoAmount = 0;
+            }
+        }
+        servo.setPosition(servoAmount);
     }
     @Override
     public void init() {
@@ -41,6 +65,9 @@ public class MotorNameTest extends OpMode {
         leftBack = hardwareMap.dcMotor.get("leftBack");
         rightFront = hardwareMap.dcMotor.get("rightFront");
         rightBack = hardwareMap.dcMotor.get("rightBack");
+        arm = hardwareMap.dcMotor.get("arm");
+        armEnd = hardwareMap.dcMotor.get("armEnd");
+        servo = hardwareMap.servo.get("servo");
         one = new Controller(gamepad1);
     }
 }
