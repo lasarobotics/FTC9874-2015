45a0d975e945c22b1fe433c02858c63bb0791dec
Russell Coleman <russellsayshi@gmail.com>
2016-01-07 16:14:13 -0600

    Removed everything

--- docs/doxygen/html/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['action',['Action',['../classcom_1_1lasarobotics_1_1library_1_1doodle_1_1actions_1_1_action.html',1,'com::lasarobotics::library::doodle::actions']]]
-];
--- docs/doxygen/html/search/classes_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['buttonstate',['ButtonState',['../classcom_1_1lasarobotics_1_1library_1_1controller_1_1_button_state.html',1,'com::lasarobotics::library::controller']]]
-];
