928c1bba44d185161e1b3144a9ddfcd21e02f1e0
Russell Coleman <russellsayshi@gmail.com>
2016-01-25 22:59:12 +0000

    Switched FTCLibrary to feature-nav branch

---
+++ ftc-library/src/main/java/com/lasarobotics/library/nav/EncodedMotor.java
@@ -0,0 +1,181 @@
+package com.lasarobotics.library.nav;
+
+import com.lasarobotics.library.util.Units;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+
+/**
+ * Drive encoder support
+ */
+public class EncodedMotor extends DcMotor {
+    protected double wheelRadius;   //wheel radius in meters
+    private boolean isEncoderEnabled = true;
+    private boolean encodersResetting = false;
+    private int encoderOffset = 0;
+
+    /**
+     * Initialize the encoded motor. You will need to run this during the init() method and run
+     * the update() method every loop().
+     *
+     * @param motor DcMotor instance.
+     */
+    public EncodedMotor(DcMotor motor, MotorInfo info) {
+        super(motor.getController(), motor.getPortNumber(), motor.getDirection());
+        enableEncoder();
+        reset();
+        this.wheelRadius = info.getEffectiveWheelRadius(Units.Distance.METERS);
+    }
+
+    protected EncodedMotor(DcMotorController controller, int portNumber, MotorInfo info) {
+        super(controller, portNumber);
+        enableEncoder();
+        reset();
+        this.wheelRadius = info.getEffectiveWheelRadius(Units.Distance.METERS);
+    }
+
+    protected EncodedMotor(DcMotorController controller, int portNumber, MotorInfo info, Direction direction) {
+        super(controller, portNumber, direction);
+        enableEncoder();
+        reset();
+        this.wheelRadius = info.getEffectiveWheelRadius(Units.Distance.METERS);
+    }
+
+    /**
+     * Move a certain distance, in encoder counts
+     * If the distance is negative, the wheel will move in reverse - forward otherwise
+     *
+     * It is assumed that there are 1440 encoder counts in a full rotation.
+     *
+     * @param distance The distance to move, in encoder counts
+     */
+    public void moveDistance(double distance) {
+        if (distance == 0)
+            return;
+
+        reset();
+
+        if (distance < 0)
+            super.setDirection(Direction.REVERSE);
+        else
+            super.setDirection(Direction.FORWARD);
+
+        setTargetPosition(Math.abs((int) distance));
+    }
+
+    public void moveDistance(double distance, Units.Distance unit) {
+        moveDistance(convertDistanceToEncoderCounts(distance, unit));
+    }
+
+    /**
+     * Checks if the robot has reached the expected encoder position.
+     * After reaching the position, it is recommended to reset the encoders.
+     *
+     * @param position Expected position, in encoder counts
+     * @return True if the robot has reached the position
+     */
+    public boolean hasReachedPosition(double position) {
+        return Math.abs(getCurrentPosition()) >= Math.abs(position);
+    }
+
+    /**
+     * Test if the encoder has reached a specific position in a unit
+     * After reaching the position, it is recommended to reset the encoders.
+     *
+     * @param position Position, in units
+     * @param unit     Unit of position
+     * @return True if reached the expected position, false otherwise
+     */
+    public boolean hasReachedPosition(double position, Units.Distance unit) {
+        return Math.abs(getCurrentPosition(unit)) >= Math.abs(position);
+    }
+
+    public void enableEncoder() {
+        //This command requires (at least ?) one execution loop to reset the encoders
+        setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+
+        isEncoderEnabled = true;
+    }
+
+    public void disableEncoder() {
+        //This command requires (at least ?) one execution loop to reset the encoders
+        setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+
+        isEncoderEnabled = false;
+    }
+
+    public boolean isEncoderEnabled() {
+        return isEncoderEnabled;
+    }
+
+    public void update() {
+        //Check for encoder reset
+        if (encodersResetting && super.getCurrentPosition() == 0) {
+            encodersResetting = false;
+            encoderOffset = 0;
+            enableEncoder();
+        }
+    }
+
+    /**
+     * Reset encoders.
+     */
+    public void reset() {
+        if (encodersResetting)
+            return;
+
+        //This command requires (at least ?) one execution loop to reset the encoders
+        setMode(DcMotorController.RunMode.RESET_ENCODERS);
+
+        encodersResetting = true;
+        encoderOffset = -super.getCurrentPosition();
+    }
+
+    public void setTargetPosition(double position, Units.Distance unit) {
+        setTargetPosition((int) convertDistanceToEncoderCounts(position, unit));
+    }
+
+    @Override
+    public int getTargetPosition() {
+        return super.getTargetPosition() - encoderOffset;
+    }
+
+    @Override
+    public void setTargetPosition(int position) {
+        super.setTargetPosition(position + encoderOffset);
+    }
+
+    public double getTargetPosition(Units.Distance unit) {
+        return convertEncoderCountsToDistance(getTargetPosition(), unit);
+    }
+
+    public double convertDistanceToEncoderCounts(double distance, Units.Distance unit) {
+        return Units.Distance.convertToAngle(distance, wheelRadius, Units.Distance.METERS,
+                unit, Units.Angle.ENCODER_COUNTS);
+    }
+
+    public double convertEncoderCountsToDistance(double encoderCounts, Units.Distance unit) {
+        return Units.Angle.convertToDistance(encoderCounts, wheelRadius, Units.Distance.METERS,
+                Units.Angle.ENCODER_COUNTS, unit);
+    }
+
+    /**
+     * Tests if the encoder has reset.
+     * Note that encoders take at least one execution loop to fully reset.
+     *
+     * @return True for reset, false for not reset.
+     */
+    public boolean hasEncoderReset() {
+        return getCurrentPosition() == 0;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        if (encodersResetting)
+            return 0;
+        return super.getCurrentPosition() + encoderOffset;
+    }
+
+    public double getCurrentPosition(Units.Distance unit) {
+        return convertEncoderCountsToDistance(getCurrentPosition(), unit);
+    }
+}
---
+++ ftc-library/src/main/java/com/lasarobotics/library/nav/PID.java
@@ -0,0 +1,146 @@
+package com.lasarobotics.library.nav;
+
+import com.lasarobotics.library.util.MathUtil;
+
+/**
+ * PID Targeting
+ */
+public class PID {
+    protected double setpoint = 0;
+    protected double processValue = 0;  // actual position (Process Value)
+    protected double error = 0;   // how much SP and PV are diff (SP - PV)
+    protected double integral = 0; // curIntegral + (error * Delta Time)
+    protected double derivative = 0;  //(error - prev error) / Delta time
+    protected double previousError = 0; // error from last time (previous Error)
+    protected double Kp = 0.2, Ki = 0.01, Kd = 1; // PID constant multipliers
+    protected double dt = 100.0; // delta time
+    protected double output = 0; // the drive amount that effects the PV.
+    protected double outputLast = 0; // last output power
+    protected double maxChange = 0; // maximum change in output per second
+    protected double minOutput = -1; // maximum change in output per second
+    protected double maxOutput = 1; // maximum change in output per second
+
+    public PID() {
+        this.Kp = 0.005;
+        this.Ki = 0;
+        this.Kd = 0;
+    }
+    public PID(double p, double i, double d) {
+        setCoefficients(p, i, d);
+    }
+
+    public void setCoefficients(double p, double i, double d) {
+        this.Kp = p;
+        this.Ki = i;
+        this.Kd = d;
+    }
+
+    public void addMeasurement(double measuredValue, double timeDelta) {
+        this.processValue = measuredValue;
+        this.dt = timeDelta;
+        update();
+    }
+
+    public double getSetpoint() {
+        return this.setpoint;
+    }
+
+    public void setSetpoint(double setpoint) {
+        this.setpoint = setpoint;
+    }
+
+    /**
+     * Gets the most recent error - the difference between the setpoint and the measured value
+     *
+     * @return setpoint - measured value
+     */
+    public double getError() {
+        return error;
+    }
+
+    /**
+     * Get the integral value, or the sum of the recent error values
+     *
+     * @return Integral value I
+     */
+    public double getIntegralValue() {
+        return integral;
+    }
+
+    /**
+     * Get the derivative value, or how much the error is changing
+     *
+     * @return The slope of the error values
+     */
+    public double getDerivativeValue() {
+        return derivative;
+    }
+
+    /**
+     * Gets the error value from the previous measurement
+     *
+     * @return The previous error value
+     */
+    public double getPreviousError() {
+        return previousError;
+    }
+
+    public double getCoefficientProportional() {
+        return Kp;
+    }
+
+    public double getCoefficientIntegral() {
+        return Ki;
+    }
+
+    public double getCoefficientDerivative() {
+        return Kd;
+    }
+
+    public double getLastTimeDelta() {
+        return dt;
+    }
+
+    public double getOutputValue() {
+        return output;
+    }
+
+    public void setOutputRange(double min, double max) {
+        this.minOutput = min;
+        this.maxOutput = max;
+    }
+
+    public double getMaxAcceleration() {
+        return maxChange;
+    }
+
+    public void setMaxAcceleration(double maxChange) {
+        this.maxChange = maxChange;
+    }
+
+    private void update() {
+        // calculate the difference between the desired value and the actual value
+        error = setpoint - processValue;
+        // track error over time, scaled to the timer interval
+        integral = integral + (error * dt);
+        // determin the amount of change from the last time checked
+        derivative = (error - previousError) / dt;
+
+        // calculate how much drive the output in order to get to the
+        // desired setpoint.
+        output = (Kp * error) + (Ki * integral) + (Kd * derivative);
+
+        //Clamp output
+        output = MathUtil.coerce(minOutput, maxOutput, output);
+
+        //Make sure the system does not move too fast
+        if ((output - outputLast) > maxChange * dt)
+            output = outputLast + (maxChange * dt);
+        else if ((outputLast - output) > maxChange * dt)
+            output = outputLast - (maxChange * dt);
+
+        // remember the error and output for the next time around.
+        previousError = error;
+        outputLast = output;
+    }
+}
